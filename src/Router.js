c9cd41044932b5c2d1aa7dc5c0a337de14eba227
D2MIN <102301865+D2MIN@users.noreply.github.com>
2024-10-22 19:48:25 +0300

    Добавил шапку для всех страниц

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import {createBrowserRouter} from "react-router-dom";
+import Home from "./App/Home/UI/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home/>,
   },
   {
-    path: '/about',
-    element: <div>About page</div>
+
   }
 ]);
   
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {router} from './Router';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router';
+import './App/GlobalStyle/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
